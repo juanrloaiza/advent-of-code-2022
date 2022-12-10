dac16acdf1a05f8e01b42075a5f09ae26e71ce0d
Juan R. Loaiza <juanrloaiza@gmail.com>
2022-12-10 12:35:30 -0500

    Refactoring to make more readable

--- 10-cathode_ray-tube/day-10.py
+++ 10-cathode_ray-tube/day-10.py
@@ -5,27 +5,29 @@ x = 1
 stack = []
 screen = [['.' for _ in range(40)] for _ in range(6)]
 
-for num, instruction in enumerate(instructions):
+for cycle, instruction in enumerate(instructions):
     stack += [0]
     if instruction.split()[0] == 'addx':
         stack += [int(instruction.split()[1])]
 
 
-def draw_CRT(x: int, x_cursor: int, y_cursor: int):
-    c = '.'
-    if x <= x_cursor + 1 and x >= x_cursor - 1:
-        c = '#'
-    screen[y_cursor][x_cursor] = c
+def draw_CRT():
     for row in screen:
         print(''.join(row))
     print('-' * 40)
 
 
+def get_character(x: int, cycle: int):
+    return '#' if x <= cycle % 40 + 1 and x >= cycle % 40 - 1 else '.'
+
+
 signal_strengths = []
-for num, instruction in enumerate(stack):
-    if num + 1 in [20, 60, 100, 140, 180, 220]:
-        signal_strengths.append(x * (num + 1))
-    draw_CRT(x, num % 40, num // 40)
+for cycle, instruction in enumerate(stack):
+    if cycle + 1 in [20, 60, 100, 140, 180, 220]:
+        signal_strengths.append(x * (cycle + 1))
+
+    screen[cycle // 40][cycle % 40] = get_character(x, cycle)
+    draw_CRT()
     x += instruction
 
 print("Part 1:", sum(signal_strengths))
