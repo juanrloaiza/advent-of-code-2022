9439ab5ae5983700760054439c63c8cbd2b79dee
Juan R. Loaiza <juanrloaiza@gmail.com>
2022-12-10 12:14:10 -0500

    Minor refactoring

--- 10-cathode_ray-tube/day-10.py
+++ 10-cathode_ray-tube/day-10.py
@@ -1,17 +1,14 @@
 with open('input.txt') as file:
     instructions = file.readlines()
 
-
-# Initialize
 x = 1
 stack = []
 screen = [['.' for _ in range(40)] for _ in range(6)]
 
 for num, instruction in enumerate(instructions):
-    if instruction.split()[0] == 'noop':
-        stack += [0]
+    stack += [0]
     if instruction.split()[0] == 'addx':
-        stack += [0, int(instruction.split()[1])]
+        stack += [int(instruction.split()[1])]
 
 
 def draw_CRT(x: int, x_cursor: int, y_cursor: int):
@@ -27,11 +24,8 @@ def draw_CRT(x: int, x_cursor: int, y_cursor: int):
 signal_strengths = []
 for num, instruction in enumerate(stack):
     if num + 1 in [20, 60, 100, 140, 180, 220]:
-        print(num + 1, x, f"({instruction})")
         signal_strengths.append(x * (num + 1))
-    x_cursor = num % 40
-    y_cursor = num // 40
-    draw_CRT(x, x_cursor, y_cursor)
+    draw_CRT(x, num % 40, num // 40)
     x += instruction
 
 print("Part 1:", sum(signal_strengths))
