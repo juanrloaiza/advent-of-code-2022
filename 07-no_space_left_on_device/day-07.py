807c935124d967608c6f13973ad1e728cdbc94e4
Juan R. Loaiza <juanrloaiza@gmail.com>
2022-12-20 21:57:24 -0500

    Minor refactoring

--- 07-no_space_left_on_device/day-07.py
+++ 07-no_space_left_on_device/day-07.py
@@ -1,7 +1,9 @@
+from dataclasses import dataclass
+
+
 class Directory():
     def __init__(self, name: str, parent: str = None):
         self.contents = {}
-        self.size = 0
         self.name = name
         self.parent = parent
 
@@ -13,16 +15,13 @@ class Directory():
         self.contents[name] = File(name, size)
 
     def get_size(self) -> int:
-        self.size = 0
-        for content in self.contents.values():
-            self.size += content.get_size()
-        return self.size
+        return sum([content.get_size() for content in self.contents.values()])
 
 
+@dataclass
 class File():
-    def __init__(self, name: str, size: str):
-        self.name = name
-        self.size = int(size)
+    name: str
+    size: int
 
     def get_size(self) -> int:
         return self.size
@@ -38,22 +37,20 @@ for line in commands:
     # Split the line in three parts.
     line_split = line.strip().split(' ')
 
-    # If the line starts with $, it's a command.
-    if line_split[0] == '$':
+    # If we are changing directory, set current directory.
+    if line_split[1] == 'cd' and line_split[2] != '/':
+        if line_split[2] == '..':
+            current_directory = current_directory.parent
+        else:
+            current_directory = current_directory.contents[line_split[2]]
 
-        # If we are changing directory, set current directory.
-        if line_split[1] == 'cd' and line_split[2] != '/':
-            if line_split[2] == '..':
-                current_directory = current_directory.parent
-            else:
-                current_directory = current_directory.contents[line_split[2]]
-
+    # Otherwise we are listing or showing files and directories.
     elif line_split[1] not in current_directory.contents:
         if line_split[0] == 'dir':
             d = current_directory.add_subdirectory(line_split[1])
             directories.append(d)
-        else:
-            current_directory.add_file(line_split[1], size=line_split[0])
+        elif line_split[0].isnumeric():
+            current_directory.add_file(line_split[1], size=int(line_split[0]))
 
 # Part 1
 less_than_limit = [d.get_size() for d in directories if d.get_size() <= 100000]
